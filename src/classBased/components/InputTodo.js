9b24680afb5a62ae2e95baf0c7137634358af2d7
Aminu Rabiu <arabiu033@gmail.com>
2022-11-01 13:56:08 +0100

    Contains the jsx for inputs and editing

---
+++ src/classBased/components/InputTodo.js
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+class InputTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { title } = this.state;
+    const { addTodoProps } = this.props;
+    if (title.trim()) {
+      addTodoProps(title);
+      this.setState({
+        title: '',
+      });
+    }
+  }
+
+  render() {
+    const { title } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
+          name="title"
+          className="input-text"
+          placeholder="Add Todo..."
+          value={title}
+          onChange={this.onChange}
+        />
+        <button className="input-submit" type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+InputTodo.propTypes = {
+  addTodoProps: PropTypes.func.isRequired,
+};
+
+export default InputTodo;
